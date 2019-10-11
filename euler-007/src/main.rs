9ad507fabfa330212aab2997d322605428b21f1c
Harrison McCullough <mccullough.harrison@gmail.com>
2019-10-11 12:58:05 -0600

    Remove commented code

--- euler-007/src/main.rs
+++ euler-007/src/main.rs
@@ -20,33 +20,6 @@ fn primes_below(limit: usize) -> Vec<usize> {
     primes
 }
 
-// fn get_factors(number: usize) -> Vec<usize> {
-//     let max_factor = (number as f64).sqrt() as usize;
-//     if number < 2 {
-//         return Vec::new();
-//     }
-//     let mut factors = Vec::new();
-//     let primes = primes_below(max_factor + 1);
-//     let mut number = number;
-//     for prime in primes {
-//         while divisible_by(number, prime) {
-//             factors.push(prime);
-//             number /= prime;
-//         }
-//         if number == 1 {
-//             break;
-//         }
-//     }
-//     if number > 1 {
-//         factors.push(number);
-//     }
-//     factors
-// }
-
-// fn divisible_by(number: usize, divisor: usize) -> bool {
-//     number % divisor == 0
-// }
-
 const FACTOR: usize = 10;
 
 fn estimate_nth_prime(nth: usize) -> usize {
